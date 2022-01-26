40668cc66580250eff631b93cc63aef19bc2de83
KassidyMab <89809905+KassidyMab@users.noreply.github.com>
2022-01-26 13:19:15 -0700

    Intial Backup of the files
    Project most likely complete at this point mostly for preservation purposes.

---
+++ ascii.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Surprised Pikachu the Meme, for those who browse reddit on the linux terminal:\n");
+    printf("----------------------------------------------------------------------------------------------\n");
+    printf("   #                                                                            #\n");
+    printf("  ###                                                                          ###\n");
+    printf("  #####                                                                       #####\n");
+    printf("  ####  #                   #########################                        ###  #\n");
+    printf("   ##    #          ##################         ##################           ##    #\n");
+    printf("    #     #      #########                                 #########       #     #\n");
+    printf("     #     #    ###                                               ###     #     #\n");
+    printf("      #     ## ###      #                              #             ### ##     #\n");
+    printf("       #      #      #  ##                          #  ##               #      #\n");
+    printf("        #            #####                          #####                     #\n"); 
+    printf("       # ##            ##                             ##                    ## #\n");
+    printf("      #                             #####                                      #\n");
+    printf("     #                               ####                                       #\n");
+    printf("     # $$$$                                                                 $$$$ #\n");
+    printf("    #$$$$$$$                       #########                               $$$$$$$$#\n");
+    printf("   #$$$$$$$$                      #$$$$$$$$$#                               $$$$$$$$#\n");
+    printf("  #$$$$$$$$                       #$$$$$$$$$#                                $$$$$$$#\n");
+    printf("  #$$$$$$$                        #$$$$$$$$$#                                 $$$$$$$#\n");
+    printf(" # $$$$$$                          #########                                    $$$$$ #\n");
+    printf("#                                                                                      #\n");
+    printf("#                                                                                      #\n");
+    printf("#                                                                                       #\n");
+    printf(" #                                                                                       #\n");
+    printf("  #                                                                                       #\n");
+    printf("   #                                                                                       #\n");
+    printf("    ##                                                                                       #\n");
+    printf("----------------------------------------------------------------------------------------------\n");
+    printf("If it the lines don't look mostly smooth it or have gaps the terminal needs to be a bigger window.\n");   
+}
---
+++ name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("K      K                      \n");
+    printf("K     K                       \n");
+    printf("K    K                        \n");
+    printf("K   K                         \n");
+    printf("K  K                          \n");
+    printf("K K                           \n");
+    printf("KK           aaaaa      ssssss\n");
+    printf("K K         a     a     s     \n");
+    printf("K  K        a     a     s     \n");
+    printf("K   K       a     a     ssssss\n");
+    printf("K    K      a     a          s\n");
+    printf("K     K     a     a          s\n");
+    printf("K      K     aaaaa a    ssssss\n");
+    
+    return 0;
+}
